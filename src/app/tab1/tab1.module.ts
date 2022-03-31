90a95f8489dae6eb514f12617044f4b8a0220d30
hachristian <59273455+hachristian@users.noreply.github.com>
2022-03-31 12:01:45 -0500

    s

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+// import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,8 +14,10 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    // ScrollingModule
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+// import {ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Tab1Page {
+  
+  tags = ["Paid", "Looking"];
+  icons = ["cash-outline", "search-circle-outline"];
+  names = [];
+  college:string;
+  department:string;
+  paid:boolean;
+  looking:boolean;
+  count:number;
 
-  constructor() {}
+  constructor() {
+    // const items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+    this.count = 0;
+    this.addNames()
+  }
+
+  search() {
+    document.getElementById("1").innerHTML = this.college;
+    document.getElementById("2").innerHTML = this.department;
+  }
+
+  remove(id: number): void {
+    this.tags.splice(id, 1);
+    this.icons.splice(id, 1);
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addNames()
+      event.target.complete();
+    }, 500);
+  }
+
+  addNames() {
+    for (let i = 0; i < 10; i++) {
+      this.names.push("John Doe " + this.count);
+      this.count += 1;
+    }
+  }
 
 }
