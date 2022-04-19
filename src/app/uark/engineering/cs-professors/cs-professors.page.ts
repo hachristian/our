ae75488a8f2221d4aa45093a58309a59bca4959a
AIMS21 <85468743+AIMS21@users.noreply.github.com>
2022-04-19 00:11:34 -0500

    CSCE Professors implmented in UARK

--- src/app/uark/engineering/cs-professors/cs-professors.page.ts
+++ src/app/uark/engineering/cs-professors/cs-professors.page.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CsProfessorsPage implements OnInit {
 
-  constructor() { }
+  names = [];
+  images = [];
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.getNamePic();
+  }
+
+  getNamePic(){
+    fetch('https://campusdata.uark.edu/apiv2/people/listFS?$filter=((Classifications+eq+%27Faculty%27)+and+(BudgetaryUnit+eq+%27csce%27))&$orderby=firstName+asc')
+    .then(result => result.json())
+    .then((output) => {
+        console.log('Output: ', output);
+        for (let i = 0; i < output.length; i++) {
+          this.names.push(output[i].firstName + " " + output[i].lastName);
+          //  this.names.push(output[i].department)
+          this.images.push("https://campusdata.uark.edu/resources/images/FacultyStaffProfile/"+output[i].image);
+        }
+    }).catch(err => console.error(err));
+  }
 }
